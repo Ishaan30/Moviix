8df6f7ec541b4bdd253b978abec22eb034cff660
Ishaan <ishaansharma1807@gmail.com>
2023-07-10 18:48:41 +0530

    making components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,57 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
 
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
-import { fetchDataFromApi } from './utils/app'
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+
 
 function App() {
-   useEffect(()=>{
-apiTesting();
-   },[]);
-   const apiTesting=()=>{
-    fetchDataFromApi("/movie/popular").then((res)=>{
-      console.log(res);
-    })
-   }
+  
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
 
-  return (
-    <>
-      app 
-       
-    </>
-  )
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
+    const fetchApiConfig = () => {
+      fetchDataFromApi("/configuration").then((res) => {
+          console.log(res);
+
+          const url = {
+              backdrop: res.images.secure_base_url + "original",
+              poster: res.images.secure_base_url + "original",
+              profile: res.images.secure_base_url + "original",
+          };
+
+          dispatch(getApiConfiguration(url));
+      });
+  };
+  
+   
+
+   return (
+    <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:mediaType/:id" element={<Details />} />
+            <Route path="/search/:query" element={<SearchResult />} />
+            <Route path="/explore/:mediaType" element={<Explore />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+);
 }
 
 export default App
